e3fa46680e8b7b7aa6e23b51c5d18c7df1ede4b6
Rahul Pandey <amviraaj2@gmail.com>
2022-08-23 14:07:00 +0530

    Add files via upload

---
+++ Chrome Extension pp/index.js
@@ -0,0 +1,64 @@
+/* Truthy or Falsy decribed here 
+console.log(Boolean(""))   - Falsy 
+console.log(Boolean("0"))   - Truthy
+console.log(Boolean(100))   - Truthy
+console.log(Boolean(null))  - Falsy
+console.log( Boolean([0]))  - Truthy
+console.log(Boolean(0))   - Falsy
+ -----*/
+
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const delbtn = document.getElementById("delete-btn")
+const tabbtn = document.getElementById("tab-btn")
+
+
+let leadsfromstorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsfromstorage){
+    myLeads = leadsfromstorage
+    renderLeads(myLeads)
+    
+}
+
+
+tabbtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true ,currentWindow : true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads" ,JSON.stringify (myLeads) )
+        renderLeads(myLeads)  
+    })
+})
+
+
+delbtn.addEventListener("dblclick",function (){
+   localStorage.clear()
+   myLeads=[]
+   renderLeads(myLeads) 
+})
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads(myLeads)
+})
+
+
+function renderLeads(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
